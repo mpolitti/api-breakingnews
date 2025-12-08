277f55f22a6923624eb79ae621b57097d92ee558
mpolitti <marcopolitti.br@gmail.com>
2025-12-08 09:13:02 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import express from 'express'
+const express = require("express");
+const userRoute = require("./src/routes/user.route");
 
-const app = express()
+const app = express();
 
-//rota
-
-  //Método HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-
-    //get - Pega uma informação
-    //post - Cria uma informação
-    //put - Altera uma informação
-    //path - Altera parte de uma informação
-    //delete - Apaga uma informação
-  
-  //Nome - Um identificador da rota
-
-  //Função (Callback) - Responsável por executar algum comando/ação
-
-app.get('/soma', (req, res) => {
-  const soma = 100 + 1;
-  res.send({soma: soma});
-})
+app.use("/soma", userRoute);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
