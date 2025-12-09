6d39a60605c926258702c14aa84c0c985319bbce
mpolitti <marcopolitti.br@gmail.com>
2025-12-09 11:08:02 -0300

    added route and controller for user.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const userRoute = require("./src/routes/user.route");
-
 const app = express();
 
-app.use("/soma", userRoute);
+const userRoute = require("./src/routes/user.route");
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+const port = 3000;
+
+app.use(express.json());
+app.use("/user", userRoute);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 })
