76e79312764491c3da908131c0aae6d9203bf44b
mpolitti <marcopolitti.br@gmail.com>
2025-12-15 07:22:13 -0300

    The use of CommonJS has been replaced with the ES module.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const userService = require("../services/user.service");
+import userService from '../services/user.service.js';
 
 const create = async (req, res) => {
 
@@ -89,7 +89,7 @@ const update = async (req, res) => {
 
 }
 
-module.exports = {
+export default {
   create,
   findAll,
   findById,
--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const connectDatabase = () => {
 
     console.log("Wait connecting to the database");
 
     mongoose
-    .connect("mongodb+srv://<db_username>:<db_password>@breakingnews.9klmym9.mongodb.net/?appName=breakingnews", 
+    .connect("mongodb+srv://<db_username>:<db_password>@breakingnews.9klmym9.mongodb.net/?appName=breakingnews",
         { serverSelectionTimeoutMS: 5000, socketTimeoutMS: 45000, }
     )
     .then(() => console.log("MongoDB Atlas Connected"))
@@ -13,4 +13,4 @@ const connectDatabase = () => {
 
 }
 
-module.exports = connectDatabase;
+export default connectDatabase;
